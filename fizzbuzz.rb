375d3a690bccc785d48de1208155234388a0255d
Catherine Price <catprice444@gmail.com>
2020-04-08 01:13:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,12 @@
 def fizzbuzz
 end
 
-      if i % 5 == 0 and i % 3 == 0
+if fizzbuzz % 5 == 0 and fizzbuzz % 3 == 0
         puts "FizzBuzz"
-      elsif i % 5 == 0
+  elsif fizzbuzz % 5 == 0
         puts "Buzz"
-      elsif i % 3 == 0
+  elsif fizzbuzz % 3 == 0
         puts "Fizz"
-      else
-        puts i
-      end
-    
+  else
+        puts "nil"
+end
