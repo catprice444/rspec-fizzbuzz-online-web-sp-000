249ce93234bda3af272b749d220ca4500d43da3c
Catherine Price <catprice444@gmail.com>
2020-04-08 01:02:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz
+end
+
+if i % 5 == 0 and i % 3 == 0
+  puts "Fizzbuzz"
+elsif i % 5 == 0
+  puts "Buzz"
+elsif i % 3 == 0
+  puts "Fizz"
+else
+  puts "nil"
+end
