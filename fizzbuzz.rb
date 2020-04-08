ffd6535faa01a3051286e95e1e8d568f5beb56b5
Catherine Price <catprice444@gmail.com>
2020-04-08 01:27:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,19 @@ def fizzbuzz
 end
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0
+    "Fizz" 
+  end
+end
+
+def fizzbuzz(int)
+  if int % 5 == 0
+    "Buzz"
+  end
+end
+
+def fizzbuzz(int)
+  if int % 3 == 0 and int % 5 ==0
+    "FizzBuzz"
   end
 end
