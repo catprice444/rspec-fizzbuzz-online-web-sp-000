808100ed848f9eac89b71a001600930bb3ee0bbf
Catherine Price <catprice444@gmail.com>
2020-04-08 01:25:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,14 +9,3 @@ def fizzbuzz(int)
   end
 end
 
-def fizzbuzz(int)
-  if int % 5 == 0
-  puts "Buzz"
-  end
-end
-
-def fizzbuzz(int)
-  if int % 3 == 0 and int % 5 == 0
-  puts "FizzBuzz"
-  end
-end
